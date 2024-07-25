e9ca3d298ead5927d9112e8bd82deacdc74c95ac
Zakaria Dorhmi <dorhmizakaria@gmail.com>
2024-07-25 16:51:43 +0100

    Navbar Added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="navbar-container">
+        <router-link to="/" class="nav-home">
+            <font-awesome-icon :icon="['fas', 'home']" />
+            <p>Home</p>
+        </router-link>
+
+        <router-link to="user" class="nav-user">
+            <font-awesome-icon :icon="['fas', 'user']" />
+            <p>User</p>
+        </router-link>
+    </div>
+</template>
+
+<script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { RouterLink} from 'vue-router'
+</script>
+
+<style>
+.navbar-container {
+    background-color: #333;
+    width: 80px;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    padding-top: 30px;
+    color: #fff;
+    box-shadow: 1px 1px 10px 2px  gray;
+}
+.nav-user {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    text-decoration: none;
+    color: #fff;
+    transition: 0.5s;
+}
+.nav-user:hover {
+    scale: 1.1;
+}
+.nav-home {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    text-decoration: none;
+    color: #fff;
+    transition: 0.5s;
+}
+.nav-home:hover {
+    scale: 1.1;
+}
+</style>
---
+++ src/views/UserView.vue
@@ -0,0 +1,11 @@
+<template>
+    <main>
+        User
+    </main>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+</style>
