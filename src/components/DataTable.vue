34f979d35df67a384db3c6eed71f1d0048472ae4
Zakaria Dorhmi <dorhmizakaria@gmail.com>
2024-07-26 00:34:21 +0100

    dataTable added

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -2,18 +2,14 @@
         <table class='user-table'>
     <thead>
         <tr>
-            <th>Email</th>
-            <th>Password</th>
+            <th class="t-head">Email</th>
+            <th class="t-head">Password</th>
         </tr>
     </thead>
     <tbody>
             <tr>
-                <td>
-                    <input class="user-input" type="text" >
-                </td>
-                <td>
-                    <input class="user-input" type="text" >
-                </td>
+                <td class="t-head">hello@gmail.com</td>
+                <td class="t-head">sdgdfggf</td>
             </tr>
     </tbody>
     </table>
@@ -23,4 +19,15 @@
 </script>
 
 <style>
+.user-table {
+    width: 100%;
+    /* border-spacing: 7px; */
+    
+}
+.t-head {
+    border: 1px solid black;
+    border-radius: 8px;
+    text-align: center;
+    padding: 3px 6px;
+}
 </style>
